d8a6faf6d9839de39d2cccb4848914bbc1861fcf
Evgeniy Antonov <zhekahead@gmail.com>
2022-06-23 05:32:37 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let n = prompt("Введите число");
+nextNum:
+for (let i = 2; i <= n; i++ ) {
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            continue nextNum;
+        }
+    }
+    alert ( i );
+}
